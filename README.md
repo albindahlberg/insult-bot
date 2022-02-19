04dba30f2527ca2943eac868a7cbf840a98a02d7
Albin Dahlberg <albin.c.dahlberg@gmail.com>
2022-02-19 12:02:38 +0100

    build setup and initial bot class

---
+++ README.md
@@ -0,0 +1,9 @@
+# Insult Discord Bot
+Insult your pals
+
+## How To:
+
+### Compile
+
+### Running
+
--- src/main/java/bot/InsultBot.java
+++ src/main/java/bot/InsultBot.java
@@ -2,12 +2,49 @@ package bot;
 
 import net.dv8tion.jda.api.JDA;
 import net.dv8tion.jda.api.JDABuilder;
+import net.dv8tion.jda.api.entities.Member;
+import net.dv8tion.jda.api.entities.Message;
+import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
+import net.dv8tion.jda.api.hooks.ListenerAdapter;
+import net.dv8tion.jda.api.requests.GatewayIntent;
 
+import java.util.List;
 
-public class InsultBot {
-    public static void main(String[] arguments) throws Exception
+/**
+ * The bot class build with JDA (Java Discord API)
+ */
+public class InsultBot extends ListenerAdapter {
+    public static void main(String[] args) throws Exception
     {
-        String BOT_TOKEN = "OTQzMjQ4MjQyNzgxNjAxODEy.YgwSYQ.1E_nPl8UVeUMJr2QOt_1fCezWJ8";
-        JDA api = JDABuilder.createDefault(BOT_TOKEN).build();
+        JDABuilder setup = JDABuilder.createDefault(args[0]).
+                addEventListeners(new InsultBot())
+                .enableIntents(GatewayIntent.GUILD_MESSAGES,
+                                GatewayIntent.GUILD_MESSAGE_TYPING,
+                                GatewayIntent.DIRECT_MESSAGE_TYPING);
+        JDA insultBot = setup.build();
+    }
+
+    private String generateInsult(){
+        return "Insult";
+    }
+
+    @Override
+    public void onMessageReceived(MessageReceivedEvent event){
+        // Do not respond to bot messages
+        if (event.getAuthor().isBot()) return;
+
+        Message msg = event.getMessage();
+        String input = msg.getContentRaw();
+        if (input.startsWith(":insult ")) {
+            List<Member> members = msg.getMentionedMembers();
+            if (!members.isEmpty()) {
+                for (Member member : members) {
+                    String insult = generateInsult();
+                    String response = member.toString() + ' ' + insult;
+                    event.getChannel().sendMessage(response).queue();
+                }
+            }
+        }
     }
 }
+
